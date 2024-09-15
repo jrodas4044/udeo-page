77c45fee12ccbb4d7c08208f1e103543c5434048
Jonhathan Rolando Rodas <jrodas4044@gmail.com>
2024-09-15 01:51:15 -0600

    add page

--- src/components/HubSpotScript.tsx
+++ src/components/HubSpotScript.tsx
@@ -1,7 +1,7 @@
 export default function HubSpotScript() {
   return (
-    <div>
+    <>
      <script>window.$zoho=window.$zoho || {};$zoho.salesiq=$zoho.salesiq||{ready:function(){}}</script><script id="zsiqscript" src="https://salesiq.zohopublic.com/widget?wc=siqc095d68cdb0e985f9d1266e7b565969896d5611595ca0163f8e3279d4ba82e29" defer></script>
-    </div>
+    </>
   );
 }
