dc198018f9352e8b910cd2ce4c760ddca304cc32
Jonhathan Rolando Rodas <jrodas4044@gmail.com>
2024-09-15 01:49:01 -0600

    add page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
         {children}
 
         <Footer />
-        {/* Agrega el componente HubSpotScript */}
+        <HubSpotScript />
       </body>
     </html>
   );
