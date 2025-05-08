9c022579426789c36018f41ca19419a1a692ca7f
Jonhathan Rolando Rodas <jrodas4044@gmail.com>
2025-05-08 09:52:37 -0600

    Actualizar el script de configuración de Zoho SalesIQ en layout.tsx para incluir un ID único.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 
         <Footer />
         <>
-          <Script>{`window.$zoho=window.$zoho || {};$zoho.salesiq=$zoho.salesiq||{ready:function(){}}`}</Script>
+          <Script id="zoho-salesiq-config">{`window.$zoho=window.$zoho || {};$zoho.salesiq=$zoho.salesiq||{ready:function(){}}`}</Script>
           <Script
             id="zsiqscript"
             src="https://salesiq.zohopublic.com/widget?wc=siqc095d68cdb0e985f9d1266e7b565969896d5611595ca0163f8e3279d4ba82e29"
